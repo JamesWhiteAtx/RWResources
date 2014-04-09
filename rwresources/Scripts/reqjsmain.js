de3b0ba7822e33503ba716c0118f58dca7c40f16
James White <jamesatlarge@yahoo.com>
2014-04-08 20:27:41 -0500

    four eight pm

--- rwresources/Scripts/reqjsmain.js
+++ rwresources/Scripts/reqjsmain.js
@@ -1,34 +1,46 @@
-﻿require.config({
-    paths: {
-        'root': '/',
-        'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min',
-        'ngRoute': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-route.min',
-        'ngResource': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-resource.min'
-    },
-    shim: {
-        ngResource: {
-            deps: ['angular'],
-            exports: 'angular'
-        },
-        ngRoute: {
-            deps: ['angular'],
-            exports: 'angular'
-        },
-        angular: {
-            exports: 'angular'
-        }
-    }
+﻿var paths = {};
+var shims = {};
+
+var jQuery = window.jQuery;
+if (jQuery) {
+    // register the current jQuery
+    define('jquery', [], function () { return jQuery; });
+} else {
+    // load if it's not available 
+    paths.jquery = '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min';
+    //shims.jquery = { exports: 'jQuery' };
+}
+
+paths.angular = '//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min';
+shims.angular = {
+    exports: 'angular'
+};
+
+paths.ngRoute = '//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-route.min';
+shims.ngRoute = {
+    deps: ['angular'],
+    exports: 'angular'
+};
+
+paths.ngResource = '//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-resource.min';
+shims.ngResource = {
+    deps: ['angular'],
+    exports: 'angular'
+};
+
+require.config({
+    paths: paths,
+    shim: shims
 });
-
-
 
 require([
     'css!bootstrap.css',
+    'jquery',
 	'angular',
 	'app',
 	'routes',
     'controllers'
-], function (bootstrap, angular, app, routes) {
+], function (bootstrap, $, angular, app, routes) {
     'use strict';
 
     //var $html = angular.element(document.getElementsByTagName('body')[0]);
